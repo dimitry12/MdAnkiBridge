63a3157180c8d1526b4cd6cd14f2ebd2aeb56634
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2025-03-29 17:27:17 -0600

    fixed new-note syncing using unnormalized content for back-side

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -142,6 +142,7 @@ def test_new_sync(history_0_collection_path, md_1_path):
     assert note.fields[0] == "heading title"
     assert (
         note.fields[1] == "\nsome content\n"
+        note.fields[1] == "some content"
     ), "Anki-side of the synced note does not include the link to anki."
 
     col.close()
